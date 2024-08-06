87a3e1770f196f7ef23385d789f031c3f9a44430
kulothungan <kulothungan.k@syrmasgs.com>
2024-08-06 19:41:25 +0530

    Add Exception Handling

--- Essencore/DbConnection.cs
+++ Essencore/DbConnection.cs
@@ -18,77 +18,111 @@ namespace Essencore
         DataTable dt;
         public string DbConnect(string barcode)
         {
-            cmd = new SqlCommand("pro_getCustomerSerialNo", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@pcbserialno", barcode);
-            con.Open();
-            var reader = cmd.ExecuteScalar();
-            con.Close();
-            return reader.ToString(); 
+            try
+            {
+                cmd = new SqlCommand("pro_getCustomerSerialNo", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@pcbserialno", barcode);
+                con.Open();
+                var reader = cmd.ExecuteScalar();
+                con.Close();
+                return reader.ToString();
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+                MessageBox.Show("Error : " + ex.Message.ToString());
+            }
         }
 
         public List<BarcodeDetails> GetBarcodeDetails(string productno)
         {
             var list = new List<BarcodeDetails>();
-            BarcodeDetails objBar;
-            cmd = new SqlCommand("pro_getPrintedValue", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            adapter=new SqlDataAdapter (cmd);
-            dt=new DataTable();
-            adapter.Fill(dt);
-            foreach (DataRow dr in dt.Rows)
+            try
             {
-                objBar=new BarcodeDetails();
-                objBar.CustomerSerialNo = Convert.ToString(dr["CustomerSerialNo"]);
-                objBar.PCBSerialNo= Convert.ToString(dr["PCBSerialNo"]);
-                objBar.ProductNo = productno; 
-                list.Add(objBar);
+                
+                BarcodeDetails objBar;
+                cmd = new SqlCommand("pro_getPrintedValue", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                adapter = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                adapter.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    objBar = new BarcodeDetails();
+                    objBar.CustomerSerialNo = Convert.ToString(dr["CustomerSerialNo"]);
+                    objBar.PCBSerialNo = Convert.ToString(dr["PCBSerialNo"]);
+                    objBar.ProductNo = productno;
+                    list.Add(objBar);
+                }
+                return list;
             }
-            return list;
+            catch(Exception ex)
+            {
+                return list;
+                MessageBox.Show("Error :" + ex.Message.ToString());
+            }
         }
 
         public List<labeltype> GetLabels() {
             var lstType = new List<labeltype>();
-            labeltype objType=new labeltype ();
-            cmd = new SqlCommand("getLabelType", con);
-            cmd.CommandType=CommandType.StoredProcedure;
-            adapter=new SqlDataAdapter (cmd);
-            dt=new DataTable();
-            adapter.Fill(dt);
-            foreach (DataRow dr in dt.Rows)
+            try
             {
-                objType = new labeltype();
-                objType.labelmasterid = Convert.ToInt32(dr["labelmasterid"]);
-                objType.labelname = Convert.ToString(dr["labeltype"]);
-                lstType.Add(objType);
+                
+                labeltype objType = new labeltype();
+                cmd = new SqlCommand("getLabelType", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                adapter = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                adapter.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    objType = new labeltype();
+                    objType.labelmasterid = Convert.ToInt32(dr["labelmasterid"]);
+                    objType.labelname = Convert.ToString(dr["labeltype"]);
+                    lstType.Add(objType);
+                }
+                return lstType;
             }
-            return lstType;
+            catch (Exception ex)
+            {
+                return lstType;
+                MessageBox.Show("Error :" + ex.Message.ToString());
+            }
 
         }
 
         public BarcodeDetails GetProductDetails(int labelid)
         {
             var barCodeDetils = new BarcodeDetails();
-            string result = string.Empty;
-            cmd = new SqlCommand("get_ProductNo", con);
-            cmd.CommandType=CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@labelid", labelid);
-            adapter=new SqlDataAdapter (cmd);
-            dt=new DataTable();
-            adapter.Fill(dt);
-            if(dt.Rows.Count > 0)
+            try
             {
-                barCodeDetils.SyrmaSGSPartno = dt.Rows[0]["SyrmaSGSPartno"].ToString();
-                barCodeDetils.WorkOrderNo = dt.Rows[0]["WorkOrderNo"].ToString();
-                barCodeDetils.CustomerPartNo = dt.Rows[0]["CustomerPartNo"].ToString();
-                barCodeDetils.Bar_Description = dt.Rows[0]["Bar_Description"].ToString();
-                barCodeDetils.WeekDetails = Convert.ToInt32(dt.Rows[0]["WeekDetails"].ToString());
-                barCodeDetils.ProductNo = dt.Rows[0]["productno"].ToString();
+                string result = string.Empty;
+                cmd = new SqlCommand("get_ProductNo", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@labelid", labelid);
+                adapter = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    barCodeDetils.SyrmaSGSPartno = dt.Rows[0]["SyrmaSGSPartno"].ToString();
+                    barCodeDetils.WorkOrderNo = dt.Rows[0]["WorkOrderNo"].ToString();
+                    barCodeDetils.CustomerPartNo = dt.Rows[0]["CustomerPartNo"].ToString();
+                    barCodeDetils.Bar_Description = dt.Rows[0]["Bar_Description"].ToString();
+                    barCodeDetils.WeekDetails = Convert.ToInt32(dt.Rows[0]["WeekDetails"].ToString());
+                    barCodeDetils.ProductNo = dt.Rows[0]["productno"].ToString();
+                }
+                //con.Open();
+                //result = Convert.ToString(cmd.ExecuteScalar());
+                //con.Close();
+                return barCodeDetils;
             }
-            //con.Open();
-            //result = Convert.ToString(cmd.ExecuteScalar());
-            //con.Close();
-            return barCodeDetils;
+            catch (Exception ex)
+            {
+                return barCodeDetils;
+                MessageBox.Show("Error :" + ex.Message.ToString());
+            }
         }
     }
 }
